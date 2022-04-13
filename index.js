d9a1d11576d9ac4aa75be7b5284f08797d777be3
Lara <i.laragurol@gmail.com>
2022-04-14 02:40:34 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+function distanceFromHqInBlocks(loc) {
+    if (loc > 42) {
+    return loc - 42; } {
+return 42 - loc;
+    }
+}
+
+function distanceFromHqInFeet(loc) {
+    return distanceFromHqInBlocks(loc) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return distanceFromHqInFeet(destination) - distanceFromHqInFeet(start);
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) < 2000) { 
+        return (distanceTravelledInFeet(start, destination) - 400 ) * 0.02 ;
+    } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+        return 25;
+    } else { 
+        return 'cannot travel that far';
+    }
+}
+
+
+
+
 // Code your solution in this file!
